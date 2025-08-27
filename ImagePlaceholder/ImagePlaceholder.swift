0a3c5e6e8284442e86b7f9c233b1f4592139fc47
Nico Prasasty S <nicoprasastysembiring@gmail.com>
2025-08-27 15:31:35 +0700

    Initial Commit

---
+++ ImagePlaceholder/ImagePlaceholder.swift
@@ -0,0 +1,9 @@
+//
+//  ImagePlaceholder.swift
+//  ImagePlaceholder
+//
+//  Created by Nico Prasasty S on 27/08/25.
+//
+
+import Foundation
+
---
+++ ImagePlaceholderTests/ImagePlaceholderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePlaceholderTests.swift
+//  ImagePlaceholderTests
+//
+//  Created by Nico Prasasty S on 27/08/25.
+//
+
+import Testing
+@testable import ImagePlaceholder
+
+struct ImagePlaceholderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
